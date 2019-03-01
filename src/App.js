8092f2172e9fad48eb690d64757da703add73004
savinaxu <jia_shijie@163.com>
2019-02-28 22:34:33 -0800

    added rental card separeate concerns.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import {Header} from './shared/Header';
+import {RentalCard} from './components/rental/RentalCard';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Header />
+        <div className='container'>
+          <section id='rentalListing'>
+            <h1 className='page-title'>Your Home All Around the World</h1>
+            <div className='row'>
+              <RentalCard />
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
