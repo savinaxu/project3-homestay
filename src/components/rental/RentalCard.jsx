7aa2b4e1a3b112ef71b20dc975225533b2962b52
savinaxu <jia_shijie@163.com>
2019-03-02 01:27:44 -0800

    add a router link.

--- src/components/rental/RentalCard.jsx
+++ src/components/rental/RentalCard.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function RentalCard(props) {
     const rental = props.rental
     return(
         <div className='col-md-3 col-xs-6'>
-            <div className='card bwm-card'>
-                <img className='card-img-top' src={rental.image} alt=''></img>
-                <div className='card-block'>
-                    <h6 className='card-subtitle'>{rental.shared ? "Shared" : "Whole" } {rental.category} &#183; {rental.city}</h6>
-                    <h4 className='card-title'>{rental.title}</h4>
-                    <p className='card-text'>${rental.dailyRate} per Night &#183; Free Cancelation</p>
-                    <a href='' className='card-link'>More Info</a>
+            <Link to={`/rentals/${rental.id}`}>
+                <div className='card bwm-card'>
+                    <img className='card-img-top' src={rental.image} alt=''></img>
+                    <div className='card-block'>
+                        <h6 className='card-subtitle'>{rental.shared ? "Shared" : "Whole" } {rental.category} &#183; {rental.city}</h6>
+                        <h4 className='card-title'>{rental.title}</h4>
+                        <p className='card-text'>${rental.dailyRate} per Night &#183; Free Cancelation</p>
+                        <a href='' className='card-link'>More Info</a>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
     )
 }
