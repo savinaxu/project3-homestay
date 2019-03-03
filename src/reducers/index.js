9caf8ad9226aef4ec2b55c071dbe0eb97103830b
savinaxu <jia_shijie@163.com>
2019-03-02 23:47:25 -0800

    redux reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import * as redux from 'redux';
+import { rentalReducer } from './rental-reducer';
+
+export const init = () => {
+    const reducer = redux.combineReducers({
+        rentals: rentalReducer
+    })
+
+    const store = redux.createStore(reducer)
+    return store
+}
