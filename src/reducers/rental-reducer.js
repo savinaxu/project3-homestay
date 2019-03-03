aaa47717f0e88a0f509876751ca856137156e0f9
savinaxu <jia_shijie@163.com>
2019-03-03 00:06:14 -0800

    redux actions

--- src/reducers/rental-reducer.js
+++ src/reducers/rental-reducer.js
@@ -1,60 +1,13 @@
-const rentals = [
-    {
-    id: 1,
-    title: "Central Apartment",
-    city: "New York",
-    street: "Times Sqaure",
-    category: "apartment",
-    image: "http://via.placeholder.com/350x250",
-    bedrooms: 3,
-    description: "Very nice apartment",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "24/12/2017"
-    },
-    {
-    id: 2,
-    title: "Central Apartment 2",
-    city: "San Francisco",
-    street: "Main street",
-    category: "condo",
-    image: "http://via.placeholder.com/350x250",
-    bedrooms: 2,
-    description: "Very nice apartment",
-    dailyRate: 12,
-    shared: true,
-    createdAt: "24/12/2017"
-    },
-    {
-    id: 3,
-    title: "Central Apartment 3",
-    city: "Bratislava",
-    street: "Hlavna",
-    category: "condo",
-    image: "http://via.placeholder.com/350x250",
-    bedrooms: 2,
-    description: "Very nice apartment",
-    dailyRate: 334,
-    shared: true,
-    createdAt: "24/12/2017"
-    },
-    {
-    id: 4,
-    title: "Central Apartment 4",
-    city: "Berlin",
-    street: "Haupt strasse",
-    category: "house",
-    image: "http://via.placeholder.com/350x250",
-    bedrooms: 9,
-    description: "Very nice apartment",
-    dailyRate: 33,
-    shared: true,
-    createdAt: "24/12/2017"
-    }
-  ]
+import { FETCH_RENTALS } from '../actions/type'
 
-export const rentalReducer = (state = rentals, action) => {
+const INITIAL_STATE = {
+    data: []
+}
+
+export const rentalReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
+        case FETCH_RENTALS :
+            return {...state, data:action.rentals}
         default:
         return state;
     }
