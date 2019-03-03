5e1e0de99d733b90ef21a7dabcae6f6b5fefa477
savinaxu <jia_shijie@163.com>
2019-03-03 01:31:41 -0800

    improve styleing.

--- src/components/rental/RentalCard.jsx
+++ src/components/rental/RentalCard.jsx
@@ -5,14 +5,13 @@ export function RentalCard(props) {
     const rental = props.rental
     return(
         <div className='col-md-3 col-xs-6'>
-            <Link to={`/rentals/${rental.id}`}>
+            <Link className="rental-detail-link" to={`/rentals/${rental.id}`}>
                 <div className='card bwm-card'>
-                    <img className='card-img-top' src={rental.image} alt=''></img>
+                    <img className='card-img-top' src={rental.image} alt='rental main image'></img>
                     <div className='card-block'>
-                        <h6 className='card-subtitle'>{rental.shared ? "Shared" : "Whole" } {rental.category} &#183; {rental.city}</h6>
+                        <h6 className={`card-subtitle ${rental.category}`}>{rental.shared ? "Shared" : "Whole" } {rental.category} &#183; {rental.city}</h6>
                         <h4 className='card-title'>{rental.title}</h4>
                         <p className='card-text'>${rental.dailyRate} per Night &#183; Free Cancelation</p>
-                        <a href='' className='card-link'>More Info</a>
                     </div>
                 </div>
             </Link>
