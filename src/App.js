34dcfa42c7929d7a0bb065a112e23da14a23645e
savinaxu <jia_shijie@163.com>
2019-03-10 20:06:36 -0700

    added manage rentals and manage bookings.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import RentalDetail from 'components/rental/rental-detail/RentalDetail';
 import RentalCreate from 'components/rental/rental-create/RentalCreate';
 import Login from 'components/login/Login';
 import { Register } from 'components/register/Register';
+import { RentalManage } from 'components/rental/rental-manage/RentalManage';
+import BookingManage from 'components/booking/booking-manage/BookingManage';
 
 import { ProtectedRoute } from 'components/shared/auth/ProtectedRoute';
 import { LoggedInRoute } from 'components/shared/auth/LoggedInRoute';
@@ -46,6 +48,8 @@ class App extends Component {
               <Route exact path="/" render={() => <Redirect to ="/rentals" /> } />
               <Route exact path="/rentals" component = {RentalListing} />
               <Route exact path='/rentals/:city/homes' component={RentalSearchListing} />
+              <ProtectedRoute exact path='/rentals/manage' component={RentalManage} />
+                <ProtectedRoute exact path='/bookings/manage' component={BookingManage} />
               <ProtectedRoute exact path='/rentals/new' component={RentalCreate} />
               <Route exact path="/rentals/:id" component = {RentalDetail} />
               <Route exact path="/login" component = {Login} />
--- src/reducers/auth-reducer.js
+++ src/reducers/auth-reducer.js
@@ -1,21 +1,33 @@
-import { LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../actions/types';
+import {
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+    LOGOUT
+} from '../actions/types';
 
 const INITIAL_STATE = {
     isAuth: false,
     errors: [],
     username: ''
 }
-  
+
 export const authReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case LOGIN_SUCCESS:
-            return Object.assign({}, state, {isAuth: true, errors: [], username: action.username});
+            return Object.assign({}, state, {
+                isAuth: true,
+                errors: [],
+                username: action.username
+            });
         case LOGIN_FAILURE:
-            return Object.assign({}, state, {errors: action.errors});
+            return Object.assign({}, state, {
+                errors: action.errors
+            });
         case LOGOUT:
-            return Object.assign({}, state, {isAuth: false, username: ''});
+            return Object.assign({}, state, {
+                isAuth: false,
+                username: ''
+            });
         default:
             return state;
     }
 }
-  
