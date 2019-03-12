46699684f36f385fe1ba2759bfb56cb72dace6f0
savinaxu <jia_shijie@163.com>
2019-03-12 00:15:13 -0700

    remove stuff

--- src/components/rental/rental-listing/RentalListing.jsx
+++ src/components/rental/rental-listing/RentalListing.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import RentalList from "./RentalList";
 
-import * as actions from 'actions';
+import * as actions from '../../../actions';
 
 class RentalListing extends Component {
 
