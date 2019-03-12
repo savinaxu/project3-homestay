31c1e9d0df135506a301b4fd936980a0305bd55f
savinaxu <jia_shijie@163.com>
2019-03-11 17:47:23 -0700

    pre-deploy.

---
+++ build/precache-manifest.7ea4c3d49613089f00dd7d8f4313ab9a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fa1edd40e4f58d8e031",
+    "url": "/static/css/main.6a7b944c.chunk.css"
+  },
+  {
+    "revision": "8fa1edd40e4f58d8e031",
+    "url": "/static/js/main.8fa1edd4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b7071517cb5d6b3444a2",
+    "url": "/static/css/2.abcaa503.chunk.css"
+  },
+  {
+    "revision": "b7071517cb5d6b3444a2",
+    "url": "/static/js/2.b7071517.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "88f45f4a2993799fdf0df4d3583f6bb2",
+    "url": "/index.html"
+  }
+];
--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const FakeDb = require('./fake-db')
 const rentalRoutes = require('./routes/rentals')
 const userRoutes = require('./routes/users')
 const bookingRoutes = require('./routes/bookings')
+const path = require("path")
+const config = require('./config');
 const PORT = process.env.PORT || 3001
 const app = express()
 
@@ -17,13 +19,24 @@ app.use('/api/rentals', rentalRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/bookings', bookingRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    const appPath = path.join(__dirname, '..', 'build')
+    app.use(express.static(appPath))
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.resolve(appPath, 'index.html'))
+    })
+}
+
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/homestay"
 
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
         .then(() => {
-            const fakeDb = new FakeDb()
-            // fakeDb.seedDb()
+            if (process.env.NODE_ENV !== "production") {
+                const fakeDb = new FakeDb()
+                // fakeDb.seedDb()
+            }
         })
         .catch(e => {
             console.log(e);
